b61696e9edd56ce581ee0dba4733740825ceb873
Harvey Delaney <harvey@harveydelaney.com>
2021-04-04 12:02:34 +1000

    feat: upgrade component library to vue 3. Introduces Storybook to the library

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import vue from "rollup-plugin-vue";
+import postcss from "rollup-plugin-postcss";
 
 import packageJson from "./package.json";
 
@@ -20,5 +21,12 @@ export default {
       sourcemap: true
     }
   ],
-  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), vue()]
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    typescript(),
+    vue(),
+    commonjs(),
+    postcss()
+  ]
 };
